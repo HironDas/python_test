4e340802a313f17cb9caec53c4456ffe43815db6
Hiron Das <hcdas.09@gmail.com>
2017-11-02 14:16:17 +0600

    the Linked list class is added

---
+++ linklist.py
@@ -0,0 +1,16 @@
+class Node:
+    '''Class representing one node in a linked list'''
+    def __init__(self, data):
+        self.data = Node(data)
+        self.next = None
+
+class LinkedList:
+    '''Class representing a linked list data structur'''
+    def __init__(self, head):
+        self.head = head
+    def add(self, head):
+        '''Add a new node to the linked list'''
+        previous_head = self.head
+        self.head = Node(data)
+        self.head.next = previous_head
+        
